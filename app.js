2d53a0e06b0d80998c932e1daa512caa992e8f43
Thrinathtalari <thrinathtalari111@gmail.com>
2026-05-04 01:20:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
- res.send('VERSION 2 🚀');
+ res.send('<h1>My Portfolio</h1>');
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
