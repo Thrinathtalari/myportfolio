bffc9e64da0114a4ef3db85a2821e16afb902dc0
Thrinathtalari <thrinathtalari111@gmail.com>
2026-05-04 01:19:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DEPLOY TEST SUCCESS 🔥');
+ res.send('VERSION 2 🚀');
 });
 
 app.listen(3000, () => {
