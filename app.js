f93ee9241d590f5e6e5b1895b6197bcb1353fbe4
Thrinathtalari <thrinathtalari111@gmail.com>
2026-05-04 01:11:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-res.send('AUTOMATION WORKING 🔥');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('DEPLOY TEST SUCCESS 🔥
+           aaya sheer');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
